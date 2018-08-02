ca6612e2f7d6549214c99ea97bb70763724a9d83
Viktoriia Sokhach <ruzgar.vik@gmail.com>
2018-08-02 11:28:22 +0200

    Building Test Framework Step9: - adding DriverUtils class (driver.manager package) - refactoring TestBase class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,24 +1,17 @@
 package tests;
 
 import driver.manager.DriverManager;
-import org.openqa.selenium.WebDriver;
+import driver.manager.DriverUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
-
-    // Note: field 'driver' changes from public to protected
-    protected WebDriver driver;
 
     @BeforeMethod
     public void beforeTest() {
-        driver = DriverManager.getWebDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //adding new action for a browser
-        driver.manage().window().maximize();
-        driver.navigate().to("http://przyklady.javastart.pl/jpetstore/");
+        DriverManager.getWebDriver();
+        DriverUtils.setInitialConfiguration();
+        DriverUtils.navigateToPage("http://przyklady.javastart.pl/jpetstore/");
     }
 
     @AfterMethod
