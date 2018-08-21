ae685def223ca2654dbeffcff09b6f879ae9e452
Viktoriia Sokhach <ruzgar.vik@gmail.com>
2018-08-21 11:12:09 +0200

    Building Test Framework Step 15. Setting various configurations of tests (from the properties file): - creating file 'configuration. properties'; - creating 4 files in package 'configuration': PropertiesLoader, ConfigurationProperties, AppProperties, LocalWebDriverProperties, TestBase; - refactoring ApplicationURLs, BrowserFactory, DriverManager.

--- src/test/java/navigation/ApplicationURLs.java
+++ src/test/java/navigation/ApplicationURLs.java
@@ -1,7 +1,10 @@
 package navigation;
 
+import configuration.AppProperties;
+
 public class ApplicationURLs {
-    public static final String APPLICATION_URL = "http://przyklady.javastart.pl/jpetstore/";
+    public static final String APPLICATION_URL = AppProperties.getAllUrl();
     public static final String TOP_MENU_PAGE_URL = APPLICATION_URL + "actions/Catalog.action";
     public static final String LOGIN_PAGE_URL = APPLICATION_URL + "actions/Account.action?signonForm=";
 }
+
